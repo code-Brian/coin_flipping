88955ab9993a82b2684585b9f12a4ec549385b34
Brian Denmark <testingarea479@gmail.com>
2022-10-18 23:52:16 -0700

    added skeleton code needing Promise

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -5,8 +5,31 @@ import spoop from '../img/spoop.jpeg'
 export const Coin = (props) => {
     const {coin, setCoin} = props
 
-    const handleSpoop = () => {
+    function tossCoin() {
+            return Math.random() > 0.5 ? "heads" : "tails";
+        }
 
+    function fiveHeadsSync() {
+            let headsCount = 0;
+            let attempts = 0;
+            while(headsCount < 5) {
+                attempts++;
+                let result = tossCoin();
+                console.log(`${result} was flipped`);
+                if(result === "heads") {
+                    headsCount++;
+                } else {
+                    headsCount = 0;
+                }
+            }
+            return `It took ${attempts} tries to flip five "heads"`;
+        }
+        console.log( fiveHeadsSync() );
+        console.log( "This is run after the fiveHeadsSync function completes" );
+
+
+    const handleSpoop = () => {
+        console.log("Get spooped lol")
     }
 
     setCoin("Hello, coin!")
